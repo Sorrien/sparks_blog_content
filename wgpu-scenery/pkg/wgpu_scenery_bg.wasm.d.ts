9a4cb57ea5789852190b29750c6456a9986fe10d
Sorrien <sorrien.sorrowind@gmail.com>
2025-07-02 15:41:24 -0400

    commit wasm packages

---
+++ wgpu-scenery/pkg/wgpu_scenery_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure45_externref_shim: (a: number, b: number, c: any) => void;
+export const closure42_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure601_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb4d2e4e9272e075f: (a: number, b: number) => void;
+export const closure697_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
